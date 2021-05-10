72f0e87ec04e8b33bf661ff7c31c4396baf3f267
Nelson Bassey <nbassey@Nelsons-MacBook-Pro.local>
2021-05-10 20:00:17 +0200

    Formate Game_spec file with rubocop and fix bad input error

--- game/game.rb
+++ game/game.rb
@@ -1,106 +1,96 @@
-require_relative "./board"
-
-
+require_relative './board'
 
 class Game
-    def initialize
-        puts "Welcome to Tic-Tac-Toe"
-        puts
-        @game_is_playing = false
-        @current_player = 1
-        @board = Board.new
-        @valid_games = {"0" => 0, "1" => 1, "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8}
-        @againts_computer = false
-    end
+  def initialize
+    puts 'Welcome to Tic-Tac-Toe'
+    puts
+    @game_is_playing = false
+    @current_player = 1
+    @board = Board.new
+    @valid_games = { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8 }
+    @againts_computer = false
+  end
 
-    attr_reader :game_is_playing, :current_player, :valid_games
+  attr_reader :game_is_playing, :current_player, :valid_games
 
+  def current_symbol
+    @current_player == 1 ? 'X' : 'O'
+  end
 
+  def valid_move?(game)
+    @valid_games.key?(game)
+  end
 
-    def current_symbol
-        return @current_player == 1 ? "X" : "O"
-    end
+  def next_player
+    @current_player = if @current_player == 1
+                        2
+                      else
+                        1
+                      end
+  end
 
+  def start
+    puts 'Enter Y to play against the computer'
+    decision = gets.chomp
+    @againts_computer = true if (decision == 'Y') || (decision == 'y')
 
-    def is_valid_move(game)
-        @valid_games.key?(game)
-    end
+    @board.draw
+    @game_is_playing = true
+    while @game_is_playing
+      puts game_turn_text
 
+      cell = get_and_validate_user_input
 
-    def next_player
-        if @current_player == 1
-            @current_player = 2
-        else
-            @current_player = 1
-        end
-    end
-
-    def start
-        puts "Enter Y to play against the computer"
-        decision = gets.chomp
-        if decision == 'Y' or decision == 'y'
-            @againts_computer = true
-        end
-
-        @board.draw
-        @game_is_playing = true
-        while @game_is_playing
-            puts self.game_turn_text
-
-            cell = self.get_and_validate_user_input
-            
-            @board.set_cell(cell, self.current_symbol)
-            @board.draw
-            self.check_winner
-            self.check_draw
-            self.next_player
-            #@game_is_playing = false
-            
-        end
+      @board.set_cell(cell, current_symbol)
+      @board.draw
+      check_winner
+      check_draw
+      next_player
+      # @game_is_playing = false
 
     end
+  end
 
-    def game_turn_text
-        return @againts_computer && @current_player == 1 ? "Computer's turn":"Player #{@current_player}'s turn [1,4,7,0]"
+  def game_turn_text
+    @againts_computer && @current_player == 1 ? "Computer's turn" : "Player #{@current_player}'s turn [1,4,7,0]"
+  end
+
+  def check_winner
+    if @board.in_winning_state(current_symbol)
+      @game_is_playing = false
+      puts "Player #{@current_player} won"
+      puts 'Game over 😎'
     end
+  end
 
-    def check_winner
-        if @board.in_winning_state(self.current_symbol)
-            @game_is_playing = false
-            puts "Player #{@current_player} won"
-            puts "Game over 😎"
-        end
+  def check_draw
+    if @board.in_draw_state
+      @game_is_playing = false
+      puts "It's a draw"
+      puts 'Game over 😎'
     end
+  end
 
-    def check_draw
-        if @board.in_draw_state
-            @game_is_playing = false
-            puts "It's a draw"
-            puts "Game over 😎"
-        end
-    end
-
-    def get_and_validate_user_input
-
-        game = gets.chomp
-        while !self.is_valid_move(game)
-            puts "Invalid move, retry"
-            game = gets.chomp
-        end
-
+  def get_and_validate_user_input
+    is_not_valid_move = true
+    is_not_valid_cell = true
+    while is_not_valid_move || is_not_valid_cell
+      # valid_move?(game) && current_cell != '' && %w[X O].include?(current_cell)
+      game = gets.chomp
+      if valid_move?(game)
+        is_not_valid_move = false
         cell = @valid_games[game]
         current_cell = @board.get_cell(cell)
-        while current_cell == 'X' || current_cell == 'O'
-            puts 'Cell is not empty, please retry'
-            game = gets.chomp
-
-            #simply this
-            cell = @valid_games[game]
-            current_cell = @board.get_cell(cell)
+        if %w[X O].include?(current_cell)
+          puts current_cell
+          puts 'Invalid cell, retry'
+        else
+          is_not_valid_cell = false
         end
-
-        cell
+      else
+        puts 'Invalid move, retry'
+      end
     end
-
+    cell
+  end
 end
-
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,43 +1,40 @@
-require "rspec"
-require_relative "../game/game"
-
+require 'rspec'
+require_relative '../game/game'
 
 RSpec.describe Game do
-    it "should initialize game with right defaults" do
-        game = Game.new
-        game.current_player.should == 1
-        game.game_is_playing.should == false
-        game.valid_games.should == {"0" => 0, "1" => 1, "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8}
-    end
+  it 'should initialize game with right defaults' do
+    game = Game.new
+    game.current_player.should == 1
+    game.game_is_playing.should == false
+    game.valid_games.should == { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
+                                 '8' => 8 }
+  end
 
-    it "should return true for valid moves" do
-        game = Game.new
-        game.is_valid_move("1").should == true
-        game.is_valid_move("2").should == true
-    end
+  it 'should return true for valid moves' do
+    game = Game.new
+    game.valid_move?('1').should == true
+    game.valid_move?('2').should == true
+  end
 
-    it "should return false for invalid moves" do
-        game = Game.new
-        game.is_valid_move("10").should == false
-        game.is_valid_move("x").should == false
-    end
+  it 'should return false for invalid moves' do
+    game = Game.new
+    game.valid_move?('10').should == false
+    game.valid_move?('x').should == false
+  end
 
-    it "should switch player" do
-        game = Game.new
-        game.current_player.should == 1
-        game.next_player()
-        game.current_player.should == 2
-        game.next_player()
-        game.current_player.should == 1
+  it 'should switch player' do
+    game = Game.new
+    game.current_player.should == 1
+    game.next_player
+    game.current_player.should == 2
+    game.next_player
+    game.current_player.should == 1
+  end
 
-    end
-
-    it "should return right symbol for right player" do
-        game = Game.new
-        game.current_symbol.should == 'X'
-        game.next_player()
-        game.current_symbol.should == 'O'
-
-    end
-    
+  it 'should return right symbol for right player' do
+    game = Game.new
+    game.current_symbol.should == 'X'
+    game.next_player
+    game.current_symbol.should == 'O'
+  end
 end
