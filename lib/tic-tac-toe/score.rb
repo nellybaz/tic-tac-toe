e960f1df5cfeab775f810b14c9b82b8cd75a2068
Nelson Bassey <nellybaz10@gmail.com>
2021-05-21 06:09:15 +0200

    Removes duplicate json parse in score class

--- lib/tic-tac-toe/score.rb
+++ lib/tic-tac-toe/score.rb
@@ -5,6 +5,7 @@ class Score
     @file = file
     @file_name = 'scores.json'
     @file_data = @file.read(@file_name)
+    @json_content = JSON.parse @file_data
   rescue StandardError => e
     print 'error from score initializer'
     print e
@@ -13,8 +14,7 @@ class Score
   attr_reader :player1, :player2, :file_name, :file_data
 
   def retrieve_content_keys(key = 'human')
-    json_content = JSON.parse @file_data
-    content = json_content[key]
+    content = @json_content[key]
     output = []
     content.each_key do |game_key|
       output << [game_key, content[game_key]]
@@ -31,11 +31,10 @@ class Score
   end
 
   def retrieve_scores
-    json_content = JSON.parse @file_data
     output = "=======\nScores\n\n"
-    json_content.each_key do |key|
+    @json_content.each_key do |key|
       output += "\n#{key.upcase}\n\n"
-      game = json_content[key]
+      game = @json_content[key]
       game.each_key do |game_key|
         output += "#{game_key}: #{game[game_key]}\n"
       end
@@ -46,11 +45,10 @@ class Score
   end
 
   def record_game_scores(key, winner_id)
-    new_content = JSON.parse @file_data
     content_keys_and_value = retrieve_content_keys(key)
     new_score = content_keys_and_value[winner_id][1] + 1
-    new_content[key][content_keys_and_value[winner_id][0]] = new_score
-    @file.write(@file_name, new_content.to_json, mode: 'w')
+    @json_content[key][content_keys_and_value[winner_id][0]] = new_score
+    @file.write(@file_name, @json_content.to_json, mode: 'w')
     new_score
   rescue StandardError => e
     puts 'Error from record statistics'
