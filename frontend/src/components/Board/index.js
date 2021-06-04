c40e4dcca11b00cb06d5fdf580a18b5dd4115426
Nelson Bassey <nellybaz10@gmail.com>
2021-06-04 14:47:29 +0200

    Add board component to the app

--- frontend/src/components/Board/index.js
+++ frontend/src/components/Board/index.js
@@ -1,10 +1,12 @@
 import Cell from "../Cell"
+import './index.css'
+
 
 export default function Board() {
   return (
-    <div data-testid='board'>{
+    <div className='board' data-testid='board'>{
       [...Array(9).keys()].map((index)=>
-        <Cell key={index} value={index} />
+        <Cell key={index} value={index} clickHandler={()=>{alert(0)}}/>
       )
     }</div>
   )
--- frontend/src/components/Cell/index.js
+++ frontend/src/components/Cell/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
+import './index.css'
 
 export default function Cell(props){
-  return <div data-testid={`cell-${props.value}`} onClick={props.clickHandler}>{props.value}</div>
+  return <div className='cell' data-testid={`cell-${props.value}`} onClick={()=>props.clickHandler(props.value)}>{props.value}</div>
 }
