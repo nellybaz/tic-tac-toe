648bbff66f43977ebce6f459941c1d256749c66f
Nelson Bassey <nellybaz10@gmail.com>
2021-06-11 12:47:40 +0200

    adds method exposing tic-tac-toe methods in the gem

--- lib/tic-tac-toe.rb
+++ lib/tic-tac-toe.rb
@@ -1,5 +1,38 @@
+require_relative './tic-tac-toe/game_text'
+require_relative './tic-tac-toe/board'
+require_relative './tic-tac-toe/game_rules'
+require_relative './tic-tac-toe/players/computer_player'
+require_relative './tic-tac-toe/players/smart_computer_player'
+
 class TicTacToe
   def self.hi
-    puts 'Hi there from tic-tac-toe'
+    puts 'Hi there from tic-tac-toe 2'
+    GameText.game_over
   end
+
+
+  def self.draw?(state)
+    board = Board.new(3)
+    board.state = state
+    GameRules.draw?(board)
+  end
+
+  def self.win?(state, symbol)
+    board = Board.new(3)
+    board.state = state
+    GameRules.win?(board, symbol)
+  end
+
+  def self.computer_move(state, player_id, player_symbol)
+    board = Board.new(3)
+    board.state = state
+    ComputerPlayer.new(player_id, player_symbol).move(board)
+  end
+
+  def self.smart_computer_move(state, player_id, player_symbol)
+    board = Board.new(3)
+    board.state = state
+    SmartComputerPlayer.new(player_id, player_symbol).move(board)
+  end
+
 end
--- tic-tac-toe.gemspec
+++ tic-tac-toe.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name        = 'tic-tac-toe'
-  s.version     = '0.0.0'
+  s.version     = '0.0.3'
   s.summary     = "Tic-tac-toe game"
   s.description = "A simple tic-tac-toe game"
   s.authors     = ["Nelson Bassey"]
   s.email       = 'nellybaz10@gmail.com'
-  s.files       = ["lib/tic-tac-toe.rb"]
+  s.files       = `git ls-files`.split("\n")
   s.homepage    =
     'https://rubygems.org/gems/hola'
   s.license       = 'MIT'
